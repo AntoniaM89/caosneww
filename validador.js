b0bd705fe7006b846ec3a0c176ec0e9c32395f38
AntoniaM89 <an.marambio@duocuc.cl>
2023-05-05 17:57:04 -0400

    FORMULARIO ACTUALIZADO

--- validador.js
+++ validador.js
@@ -4,45 +4,45 @@ var mensaje = document.getElementById("mensaje");
 
 // Permitir sólo números en el imput
 function isNumber(evt) {
-    var charCode = evt.which ? evt.which : event.keyCode;
-    if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode === 75) return false;
+  var charCode = evt.which ? evt.which : event.keyCode;
+  if (charCode > 31 && (charCode < 48 || charCode > 57) && charCode === 75) return false;
 
-    return true;
+  return true;
 }
 
 function checkRut(rut) {
 
-    if (rut.value.length <= 1) {
+  if (rut.value.length <= 1) {
     alerta.classList.remove('alert-success', 'alert-danger');
     alerta.classList.add('alert-info');
     mensaje.innerHTML = 'Ingrese un RUT en el siguiente campo de texto para validar si es correcto o no';
-    }
+  }
 
   // Obtiene el valor ingresado quitando puntos y guión.
-    var valor = clean(rut.value);
+  var valor = clean(rut.value);
 
   // Divide el valor ingresado en dígito verificador y resto del RUT.
-    cuerpo = valor.slice(0, -1);
-    dv = valor.slice(-1).toUpperCase();
+  cuerpo = valor.slice(0, -1);
+  dv = valor.slice(-1).toUpperCase();
 
   // Separa con un Guión el cuerpo del dígito verificador.
-    rut.value = format(rut.value);
+  rut.value = format(rut.value);
 
   // Si no cumple con el mínimo ej. (n.nnn.nnn)
-    if (cuerpo.length < 7) {
+  if (cuerpo.length < 7) {
     rut.setCustomValidity("RUT Incompleto");
     alerta.classList.remove('alert-success', 'alert-danger');
     alerta.classList.add('alert-info');
     mensaje.innerHTML = 'Ingresó un RUT muy corto, el RUT debe ser mayor a 7 Dígitos. Ej: x.xxx.xxx-x';
     return false;
-    }
+  }
 
   // Calcular Dígito Verificador "Método del Módulo 11"
-    suma = 0;
-    multiplo = 2;
+  suma = 0;
+  multiplo = 2;
 
   // Para cada dígito del Cuerpo
-    for (i = 1; i <= cuerpo.length; i++) {
+  for (i = 1; i <= cuerpo.length; i++) {
     // Obtener su Producto con el Múltiplo Correspondiente
     index = multiplo * valor.charAt(cuerpo.length - i);
 
@@ -51,33 +51,55 @@ function checkRut(rut) {
 
     // Consolidar Múltiplo dentro del rango [2,7]
     if (multiplo < 7) {
-        multiplo = multiplo + 1;
+      multiplo = multiplo + 1;
     } else {
-        multiplo = 2;
-        }
+      multiplo = 2;
     }
+  }
 
   // Calcular Dígito Verificador en base al Módulo 11
-    dvEsperado = 11 - (suma % 11);
+  dvEsperado = 11 - (suma % 11);
 
   // Casos Especiales (0 y K)
-    dv = dv == "K" ? 10 : dv;
-    dv = dv == 0 ? 11 : dv;
+  dv = dv == "K" ? 10 : dv;
+  dv = dv == 0 ? 11 : dv;
 
   // Validar que el Cuerpo coincide con su Dígito Verificador
-    if (dvEsperado != dv) {
+  if (dvEsperado != dv) {
     rut.setCustomValidity("RUT Inválido");
 
+    alerta.classList.remove('alert-info', 'alert-success');
+    alerta.classList.add('alert-danger');
+    mensaje.innerHTML = 'El RUT ingresado: ' + rut.value + ' Es <strong>INCORRECTO</strong>.';
 
     return false;
-    } else {
-        
+  } else {
+    rut.setCustomValidity("");
+    
 
-
+    alerta.classList.remove('d-none', 'alert-danger');
+    alerta.classList.add('alert-success');
+    mensaje.innerHTML = 'El RUT ingresado: ' + rut.value + ' Es <strong>CORRECTO</strong>.';
     return true;
-    }
+  }
 }
 
+function format (rut) {
+  rut = clean(rut)
+
+  var result = rut.slice(-4, -1) + '-' + rut.substr(rut.length - 1)
+  for (var i = 4; i < rut.length; i += 3) {
+    result = rut.slice(-3 - i, -i) + '.' + result
+  }
+
+  return result
+}
+
+function clean (rut) {
+  return typeof rut === 'string'
+    ? rut.replace(/^0+|[^0-9kK]+/g, '').toUpperCase()
+    : ''
+}
 function format (rut) {
     rut = clean(rut)
 
@@ -239,3 +261,4 @@ function SHA1(msg) {
 
   return temp.toLowerCase();
 }
+checkRut
